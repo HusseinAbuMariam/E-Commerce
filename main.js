00cba46b60eefce0092b1f938d75cd1c14161887
HusseinAbuMariam <huseinmohamaad@gmail.com>
2025-05-29 12:34:02 +0200

    E-commerce

---
+++ main.js
@@ -0,0 +1,17 @@
+let shopBtn=document.querySelector("#shop-btn")
+let buyBtn1=document.querySelector("#Buy-btn1")
+let buyBtn2=document.querySelector("#Buy-btn2")
+let buyBtn3=document.querySelector("#Buy-btn3")
+shopBtn.addEventListener("click",function(){
+    window.location.href="product.html"
+})
+buyBtn1.addEventListener("click",function(){
+    window.location.href="product.html"
+})
+buyBtn2.addEventListener("click",function(){
+    window.location.href="product.html"
+})
+
+buyBtn3.addEventListener("click",function(){
+    window.location.href="product.html"
+})
